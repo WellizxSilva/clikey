bae2f2728ea889f285cfecc5dbf8e9ff02ff7196
wellington silva <wellizxsilva@gmail.com>
2025-07-27 20:50:44 -0300

    fix

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import clikey from "./src/clikey.js";
 
-const chave = clikey.createKey({length: 64, prefix: "clikey", suffix: "v1.0", repeatCount: 12, styledOutput: true})
+const chave = clikey.createKey({
+  length: 64,
+  prefix: "clikey",
+  suffix: "v1.0",
+  repeatCount: 12,
+  styledOutput: true,
+});
 
-
-clikey.ge
-//console.log(clikey.getKeysFromResult(chave))
-
+console.log(clikey.getKeysFromResult(chave));
